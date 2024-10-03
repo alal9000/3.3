f332a0f104167cec34df7be76f90cd0b2e4ad867
Aaron Lal <lucidlear@gmail.com>
2024-10-03 18:38:57 +1000

    init commit

---
+++ Clock.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public class Clock
+    {
+        // fields
+        private Counter _seconds;
+        private Counter _minutes;
+        private Counter _hours;
+
+        // constructor
+        public Clock(string name)
+        {
+            Seconds = new Counter(name + " seconds");
+            Minutes = new Counter(name + " minutes");
+            Hours = new Counter(name + " hours");
+        }
+
+        // properties
+        public Counter Seconds { get => _seconds; set => _seconds = value; }
+        public Counter Minutes { get => _minutes; set => _minutes = value; }
+        public Counter Hours { get => _hours; set => _hours = value; }
+
+        // methods
+        public void Tick()
+        {
+            Seconds.Increment();
+
+            if (Seconds.Ticks == 60)
+            {
+                Seconds.Reset();
+                Minutes.Increment();
+            }
+
+            if (Minutes.Ticks == 60)
+            {
+                Minutes.Reset();
+                Hours.Increment();
+            }
+
+            if (Hours.Ticks == 24)
+            {
+                this.Reset();
+            }
+        }
+
+        public void Reset()
+        {
+            Seconds.Reset();
+            Minutes.Reset();
+            Hours.Reset();
+        }
+
+        public string GetCurrentTime()
+        {
+            return string.Format("{0:D2}:{1:D2}:{2:D2}", 
+                Hours.Ticks, 
+                Minutes.Ticks, 
+                Seconds.Ticks
+            );          
+        }
+
+
+    }
+}
---
+++ TestClock.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public class TestClock
+    {
+        // arrange
+        Clock clock;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            clock = new Clock("testClock");
+        }
+
+        // act and assert
+        [Test]
+        public void TestInitialise()
+        {
+            Assert.That(clock.Seconds.Ticks, Is.EqualTo(0));
+            Assert.That(clock.Minutes.Ticks, Is.EqualTo(0));
+            Assert.That(clock.Hours.Ticks, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestTick()
+        {
+            clock.Tick();
+            Assert.That(clock.Seconds.Ticks, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestReset()
+        {
+            clock.Reset();
+            Assert.That(clock.Seconds.Ticks, Is.EqualTo(0));
+            Assert.That(clock.Minutes.Ticks, Is.EqualTo(0));
+            Assert.That(clock.Hours.Ticks, Is.EqualTo(0));
+
+        }
+
+        [Test]
+        public void GetCurrentTime()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                clock.Tick();
+                
+            }
+            string currentTime = clock.GetCurrentTime();
+            Assert.That(currentTime, Is.EqualTo("00:00:30"));
+        }
+
+
+
+    }
+}
